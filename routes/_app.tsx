99dc8513b174e913555fcf780a235dd3f5361a5c
Faiq Najib <34987164+fanajib5@users.noreply.github.com>
2025-03-20 05:55:30 +0700

    feat(seo): add SEO meta tags and favicon to _app.tsx
    Add SEO meta tags using `generateMetaTags` utility and include a favicon in the application. This improves the website's SEO and provides a better user experience by displaying a favicon in the browser tab.

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,12 +1,20 @@
 import { type PageProps } from "$fresh/server.ts";
-export default function App({ Component }: PageProps) {
+import { defaultSEO, generateMetaTags } from "../utils/seo.ts";
+
+export default function App({ Component, url }: PageProps) {
+  const metaTags = generateMetaTags();
+
   return (
-    <html>
+    <html lang="id">
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>akordium-id</title>
-        
+        <title>{defaultSEO.title}</title>
+        {metaTags.map((tag) => <meta
+          {...tag}
+          key={tag.name || tag.property}
+        />)}
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         <Component />
